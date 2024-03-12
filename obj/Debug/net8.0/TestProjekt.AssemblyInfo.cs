e7d5d45f5784b0151fad05faaa5ee1fda5ad1972
fmudri <filipmudri@protonmail.com>
2024-03-12 21:43:07 +0100

    Initial commit

--- obj/Debug/net8.0/TestProjekt.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProjekt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProjekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+329ac077879e118bb2dbc55809f4edfc1c70cf05")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProjekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProjekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
