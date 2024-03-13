1ad9fba0ef3e1dbb343c3b558d54342325e4b1ca
fmudri <filipmudri@protonmail.com>
2024-03-13 22:32:40 +0100

    Made a pettingZoo file that I do not yet understand

--- obj/Debug/net8.0/TestProjekt.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProjekt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProjekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+756dc6006352630d99401f7dacc8765a03b34505")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProjekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProjekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
